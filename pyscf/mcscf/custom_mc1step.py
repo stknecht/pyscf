709685e1b3ad7cde4e368ce1a4c3ff3a22f1fd2e
afitzpa6 <107189470+afitzpa6@users.noreply.github.com>
2022-08-02 15:11:48 +0100

    updated imports again, should now work
    changed import path of custom_casci

--- pyscf/mcscf/custom_mc1step.py
+++ pyscf/mcscf/custom_mc1step.py
@@ -8,8 +8,8 @@ from pyscf import lib
 from pyscf.lib import logger
 
 
-from pyscf.pyscf.mcscf import custom_casci as casci
-from pyscf.mcscf.casci import get_fock, cas_natorb, canonicalize
+from pyscf.mcscf import custom_casci as casci
+from pyscf.mcscf.custom_casci import get_fock, cas_natorb, canonicalize
 from pyscf.mcscf import mc_ao2mo
 from pyscf.mcscf import chkfile
 from pyscf import ao2mo
